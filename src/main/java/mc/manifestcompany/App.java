61c3f37adddce768ff7ce08b774355d0b2eb5223
VoravichS <vsilapac@gmail.com>
2023-04-05 23:42:18 -0400

    init commit

--- src/main/java/mc/manifestcompany/App.java
+++ src/main/java/mc/manifestcompany/App.java
@@ -16,7 +16,6 @@ public class App extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-
     }
 
     public static void main(String[] args) {
