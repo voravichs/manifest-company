cf209e712496c07352d41c8a1cefca5e92d08d58
VoravichS <vsilapac@gmail.com>
2023-04-05 23:50:23 -0400

    init commit

--- src/main/java/mc/manifestcompany/App.java
+++ src/main/java/mc/manifestcompany/App.java
@@ -6,13 +6,22 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
+/**
+ * Starts the application, initializing the FXML and CSS files.
+ * @author VoravichS
+ */
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader =
-                new FXMLLoader(App.class.getResource("hello-view.fxml"));
+                new FXMLLoader(App.class.getResource("root.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        String css = Objects.requireNonNull(
+                App.class.getResource("stylesheet.css")).toExternalForm();
+        scene.getStylesheets().add(css);
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
