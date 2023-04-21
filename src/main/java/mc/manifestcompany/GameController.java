7648ac7f6664fa36b19ce7f9993baf4b7758aa0c
VoravichS <vsilapac@gmail.com>
2023-04-20 21:49:44 -0400

    added functionality to see data and make investing actions

--- src/main/java/mc/manifestcompany/GameController.java
+++ src/main/java/mc/manifestcompany/GameController.java
@@ -1,11 +1,12 @@
 package mc.manifestcompany;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
@@ -13,6 +14,7 @@ import javafx.scene.text.TextFlow;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.net.URL;
+import java.util.Stack;
 
 /**
  * Controls the GUI elements of the game screen.
@@ -25,6 +27,18 @@ public class GameController {
     public static final int Y_SIZE = 20;
 
     /* FXML Variables */
+    // StackPane Layers
+    @FXML
+    private BorderPane gamePane;
+    @FXML
+    private Pane dataPane;
+    @FXML
+    private Pane actionPane;
+    @FXML
+    private Pane investPane;
+    @FXML
+    private Pane transitionPane;
+
     @FXML
     private Pane gameBoard;
     @FXML
@@ -32,15 +46,15 @@ public class GameController {
     @FXML
     private GridPane sideBar;
     @FXML
-    private Pane transitionPane;
-    @FXML
-    private StackPane root;
+    private Text date;
 
     /* Instance Variables */
     private final Game game;
+    private Stack<Text> textStack;
 
     public GameController() {
         this.game = new Game(X_SIZE, Y_SIZE);
+        this.textStack = new Stack<>();
     }
 
     /**
@@ -50,7 +64,12 @@ public class GameController {
     protected void init() throws FileNotFoundException {
         initSidebar();
         updateGrid();
-        root.getChildren().remove(transitionPane);
+        Text startext = new Text("Welcome to Manifest Company!\n");
+        textBox.getChildren().add(startext);
+        textStack.push(startext);
+        date.setText("January 1970");
+        transitionPane.setVisible(false);
+        gamePane.setVisible(true);
     }
 
     /**
@@ -59,18 +78,43 @@ public class GameController {
     @FXML
     protected void initSidebar() throws FileNotFoundException {
         int i = 0;
+
         for (FastFoodLevel level:
              FastFoodLevel.values()) {
+            // Image
             URL imagePath = App.class.getResource(level.getImageLink());
             assert imagePath != null;
             Image image = new Image(new FileInputStream(imagePath.getPath()));
             ImageView imageView = new ImageView(image);
             imageView.setFitHeight(100);
             imageView.setFitWidth(100);
-            imageView.setTranslateX(12.5);
+            imageView.setTranslateX(18.75);
             sideBar.add(imageView,0,i);
+
+            // init company stats
+            // TODO: FILL THESE IN WITH THE REAL STATS
+            updateCompanyStats(i);
+
             i++;
         }
+    }
+
+    /**
+     * Updates the company stats at the sidebar.
+     * @param row row of the company to update
+     */
+    public void updateCompanyStats(int row) {
+        // TODO: REAL STATS
+        TextFlow tf = new TextFlow();
+        tf.setPadding(new Insets(20,20,20,20));
+        tf.getStyleClass().add("stats-text");
+        Text text = new Text("""
+                Net Worth: 0
+                Profit: 0
+                PLACEHOLDER: 0""");
+        tf.getChildren().add(text);
+
+        sideBar.add(tf,1,row);
     }
 
     /**
@@ -102,6 +146,60 @@ public class GameController {
     }
 
     /**
+     * Shows the data pane.
+     */
+    @FXML
+    protected void showData() {
+        gamePane.setOpacity(0.3);
+        dataPane.setVisible(true);
+    }
+
+    /**
+     * Shows the data pane.
+     */
+    @FXML
+    protected void closeData() {
+        gamePane.setOpacity(1);
+        dataPane.setVisible(false);
+    }
+
+    /**
+     * Shows the action pane.
+     */
+    @FXML
+    protected void showActions() {
+        gamePane.setOpacity(0.3);
+        actionPane.setVisible(true);
+    }
+
+    /**
+     * Closes the action pane.
+     */
+    @FXML
+    protected void closeActions() {
+        gamePane.setOpacity(1);
+        actionPane.setVisible(false);
+    }
+
+    /**
+     * Shows investing pane.
+     */
+    @FXML
+    protected void showInvest() {
+        actionPane.setOpacity(0.3);
+        investPane.setVisible(true);
+    }
+
+    /**
+     * Closes investing pane.
+     */
+    @FXML
+    protected void closeInvest() {
+        actionPane.setOpacity(1);
+        investPane.setVisible(false);
+    }
+
+    /**
      * Advances to the next turn
      */
     @FXML
@@ -110,7 +208,10 @@ public class GameController {
         System.out.println("Advancing Turn...");
         Text advanceText = new Text("Advancing Turn...\n");
         textBox.getChildren().add(advanceText);
+        textStack.push(advanceText);
     }
+
+
 
 
 }
