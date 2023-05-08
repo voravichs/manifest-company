242bf9fd3c0ea18d4762652bf8b13495b9760ae2
peiciqiu <peiciqiu@seas.upenn.edu>
2023-05-08 18:15:11 -0400

    user company test updated

--- src/main/java/mc/manifestcompany/company/UserCompanyTest.java
+++ src/main/java/mc/manifestcompany/company/UserCompanyTest.java
@@ -16,6 +16,8 @@ import static org.junit.Assert.*;
 public class UserCompanyTest {
     private Company company;
 
+    private CompanyAction actions;
+
     private Tile[][] grid;
     @Before
     public void setUp() {
@@ -23,6 +25,8 @@ public class UserCompanyTest {
                 Tile.TileType.CLAIMED_P1, "images/playerfastfood.png");
 
         this.grid = new Tile[20][20];
+
+        this.actions = new CompanyActionImpl();
 
         for (int i = 0; i < 20; i++) {
             for (int j = 0; j < 20; j++) {
@@ -162,5 +166,32 @@ public class UserCompanyTest {
         profit.add(30);
         profit.add(60);
         assertEquals(Arrays.asList(rev, cogs, profit), this.company.getFinancialHistory());
+    }
+
+    @Test
+    public void getTileStack() {
+
+        this.actions.tiles(1 , "Purchase", this.company, this.grid);
+        Stack<Point2D> tileStack = this.company.getTileStack();
+        assertEquals(tileStack.size(), 1);
+
+    }
+
+    @Test
+    public void addToStack() {
+        this.actions.tiles(1 , "Purchase", this.company, this.grid);
+        Stack<Point2D> tileStack = this.company.getTileStack();
+        assertEquals(tileStack.size(), 1);
+
+    }
+
+    @Test
+    public void popFromStack() {
+        this.actions.tiles(1 , "Purchase", this.company, this.grid);
+        Stack<Point2D> tileStack = this.company.getTileStack();
+        assertEquals(tileStack.size(), 1);
+        this.actions.tiles(-1 , "Sell", this.company, this.grid);
+        assertEquals(tileStack.size(), 0);
+
     }
 }
